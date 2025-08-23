d7e712cd3c718864ea6b22d681763db2f46379c7
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-08-23 18:23:21 +0530

    perf: Improve origin checking code

--- src/app.js
+++ src/app.js
@@ -1,18 +1,28 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import appConfig from "./config/appConfig.js";
 
 const app = express();
+const allowedOrigins = appConfig.ORIGIN.split(",");
 
 app.use(
     cors({
-        origin: process.env.CORS_ORIGIN,
+        origin: (origin, callback) => {
+            if (!origin) return callback(null, true);
+
+            if (allowedOrigins.includes(origin)) {
+                callback(null, true);
+            } else {
+                callback(new Error("❌❌ Not allow by CORS."));
+            }
+        },
         credentials: true,
+        methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     }),
 );
 
 app.use(express.json({ limit: "16kb" }));
-
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 app.use(express.static("public"));
 app.use(cookieParser());
