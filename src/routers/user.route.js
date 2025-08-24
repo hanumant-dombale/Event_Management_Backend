2f7529b7ae38012f2dcf1da8c40145642314b844
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-08-24 19:03:27 +0530

    feat: Add all user routes & function

--- src/routers/user.route.js
+++ src/routers/user.route.js
@@ -1,8 +1,27 @@
 import { Router } from "express";
-import { register } from "../controller/user.controller.js";
+import {
+    registerUser,
+    login,
+    logout,
+    updateUser,
+    forgotPassword,
+    getAllUser,
+    getUserById,
+    deleteUser,
+} from "../controller/user.controller.js";
 
 const router = Router();
 
-router.route("/register").post(register);
+router.route("/get-user-by-id/:id").get(getUserById);
+router.route("/get-all-user").get(getAllUser);
+
+router.route("/forgot-password/:id").post(forgotPassword);
+router.route("/register").post(registerUser);
+router.route("/logout").post(logout);
+router.route("/login").post(login);
+
+router.route("/update/:id").put(updateUser);
+
+router.route("/delete/:id").delete(deleteUser);
 
 export default router;
