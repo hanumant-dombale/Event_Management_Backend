601eb2c1110780cbc2746c3d0b590c59f209e671
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-08-29 16:41:57 +0530

    refactor: Exports itself

--- src/middlewares/errorHandlers.middleware.js
+++ src/middlewares/errorHandlers.middleware.js
@@ -1,7 +1,7 @@
 import appConfig from "../config/appConfig.js";
 import CustomError from "../utils/customError.js";
 
-const notFound = (req, res, next) => {
+export const notFound = (req, res, next) => {
     const err = new CustomError(
         `Can't found ${req.method}: ${req.originalUrl} resource.`,
         404,
@@ -27,7 +27,7 @@ const response = {
     },
 };
 
-const globalErrorHandle = (err, req, res, next) => {
+export const globalErrorHandle = (err, req, res, next) => {
     if (appConfig.NODE_ENV === "development") {
         return response.dev(res, err);
     }
@@ -35,10 +35,8 @@ const globalErrorHandle = (err, req, res, next) => {
     return response.prod(res, err);
 };
 
-const asyncErrorHandler = (func) => {
+export const asyncErrorHandler = (func) => {
     return (req, res, next) => {
         func(req, res, next).catch((err) => next(err));
     };
 };
-
-export { notFound, globalErrorHandle, asyncErrorHandler };
