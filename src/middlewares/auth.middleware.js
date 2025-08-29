acaa52f09b85d2f2198adfe4738a01dcb37bf7f9
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-08-29 14:00:23 +0530

    feat: Add authentication & authorization middleware

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,34 @@
+import CustomError from "../utils/customError";
+import jwt from "jsonwebtoken";
+import { asyncErrorHandler } from "./errorHandlers.middleware";
+
+// ✅ Authentication middleware
+export const authenticate = asyncErrorHandler(async (req, res, next) => {
+    const token = req.cookies.token;
+    if (!token) {
+        throw new CustomError("User not Authenticated.", 401);
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    if (!decoded) {
+        throw new CustomError("Invailed token", 401);
+    }
+
+    req.user = decoded;
+    next();
+});
+
+// ✅ Role-based authorization middleware
+export const authorizeRoles = asyncErrorHandler(async (...allowedRoles) => {
+    return (req, res, next) => {
+        if (!req.user) {
+            throw new CustomError("Not authenticated", 401);
+        }
+
+        if (!allowedRoles.includes(req.user.role)) {
+            throw new CustomError("Not valid role", 403);
+        }
+
+        next();
+    };
+});
