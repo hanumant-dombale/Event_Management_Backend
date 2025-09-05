ac878244661bcb834b4e4bedb29f98b6b74008fa
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-09-05 17:25:17 +0530

    feat: Add new env variables

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -14,6 +14,8 @@ export default {
     PGUSER: process.env.PGUSER,
     PGPASSWORD: process.env.PGPASSWORD,
     PGPORT: process.env.PGPORT | 5432,
+    PGSSLMODE: process.env.PGSSLMODE,
+    PGCHANNELBINDING: process.env.PGCHANNELBINDING,
 
     JWT_SECRET: process.env.JWT_SECRET,
     JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN | "2d",
