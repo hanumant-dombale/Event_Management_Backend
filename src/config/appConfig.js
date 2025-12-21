eb31e015d13c809b2b5d1bb305f407fed699d720
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-12-21 17:26:08 +0530

    feat: Add cloudinary & multer as well as env variables

--- src/config/appConfig.js
+++ src/config/appConfig.js
@@ -5,7 +5,7 @@ DotenvFlow.config();
 export default {
     NODE_ENV: process.env.NODE_ENV,
 
-    PORT: process.env.PORT | 6000,
+    PORT: process.env.PORT || 6000,
     ORIGIN: process.env.ORIGIN,
 
     DIALECT: process.env.DIALECT,
@@ -13,10 +13,14 @@ export default {
     PGDATABASE: process.env.PGDATABASE,
     PGUSER: process.env.PGUSER,
     PGPASSWORD: process.env.PGPASSWORD,
-    PGPORT: process.env.PGPORT | 5432,
+    PGPORT: process.env.PGPORT || 5432,
     PGSSLMODE: process.env.PGSSLMODE,
     PGCHANNELBINDING: process.env.PGCHANNELBINDING,
 
     JWT_SECRET: process.env.JWT_SECRET,
-    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN | "2d",
+    JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN || "2d",
+
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
+    CLOUDINARY_API_KEY: process.env.CLOUDINARY_API_KEY,
+    CLOUDINARY_API_SECRET: process.env.CLOUDINARY_API_SECRET,
 };
