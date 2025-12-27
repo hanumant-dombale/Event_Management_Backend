a7d9e16e6f848e156468792dea4cf1d8c8995a05
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-12-27 17:46:45 +0530

    feat: Add Change password route

--- src/routers/auth.route.js
+++ src/routers/auth.route.js
@@ -1,9 +1,15 @@
 import { Router } from "express";
-import { loginUser, logoutUser } from "../controllers/auth.controller.js";
+import {
+  changePassword,
+  loginUser,
+  logoutUser,
+} from "../controllers/auth.controller.js";
+import { authenticate } from "../middlewares/auth.middleware.js";
 
 const router = Router();
 
 router.route("/login").post(loginUser);
 router.route("/logout").post(logoutUser);
+router.patch("/change-password", authenticate, changePassword);
 
 export default router;
