4a99c1367f475f8c6d5866d3da418df008057de6
Hanumamt Dombale <hanumantdombale60@gmail.com>
2025-12-27 23:48:22 +0530

    refactor: Change code implementation

--- src/app.js
+++ src/app.js
@@ -11,11 +11,11 @@ app.use(
     origin: (origin, callback) => {
       if (!origin) return callback(null, true);
 
-      if (allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("❌❌ Not allow by CORS."));
-      }
+      if (allowedOrigins.includes("*")) return callback(null, origin);
+
+      if (allowedOrigins.includes(origin)) return callback(null, origin);
+
+      return callback(new Error("❌ Not allowed by CORS"));
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
